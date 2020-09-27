239c2803b4836a6e21dad0d4f4894da748cf85f9
sarath <sarathdon9245@gmail.com>
2020-09-27 13:48:03 +0530

    Updates

---
+++ precache-manifest.f62ab6b03ed118164d0469b3c5ed9a7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ba0a1868ae24c56368a18a3ad7c00b",
+    "url": "/break-timer/index.html"
+  },
+  {
+    "revision": "6d9c1bd6338c6a1a894e",
+    "url": "/break-timer/static/css/2.8c7c1c4d.chunk.css"
+  },
+  {
+    "revision": "e13851d17a6902532aac",
+    "url": "/break-timer/static/css/main.88bce4cd.chunk.css"
+  },
+  {
+    "revision": "6d9c1bd6338c6a1a894e",
+    "url": "/break-timer/static/js/2.fd23c1be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/break-timer/static/js/2.fd23c1be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13851d17a6902532aac",
+    "url": "/break-timer/static/js/main.30c26abc.chunk.js"
+  },
+  {
+    "revision": "e4bc41e7f3fd0e81efb8",
+    "url": "/break-timer/static/js/runtime-main.a747208a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/break-timer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/break-timer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/break-timer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/break-timer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/break-timer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
